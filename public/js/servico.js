69bcbdfec8cd1822c304478265556be103d2702e
mazashi <duduandrade94@gmail.com>
2018-04-22 00:38:06 -0300

    Materialize e Jquery

---
+++ public/js/servico.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+console.log("carregou");
+
+$(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+      data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://placehold.it/250x250'
+      },
+    });
+  });
