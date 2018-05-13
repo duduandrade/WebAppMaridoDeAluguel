bd62d98957dde10b0be33a2b9ec481488933951d
mazashi <duduandrade94@gmail.com>
2018-05-13 01:49:23 -0300

    Layoyt home logado

---
+++ public/js/homeLogado.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+
+
+
+    $('.tooltipped').tooltip();
+
+    $('.sidenav').sidenav();
+});
