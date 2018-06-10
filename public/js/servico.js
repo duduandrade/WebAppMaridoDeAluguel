dbcdc5edd8fb3449e15eeef1c8cdc241f3da053c
mazashi <duduandrade94@gmail.com>
2018-06-10 14:08:23 -0300

    Solicitar serviço - escolher serviço pré definido

--- public/js/servico.js
+++ public/js/servico.js
@@ -5,12 +5,40 @@
  */
 console.log("carregou");
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('input.autocomplete').autocomplete({
-      data: {
-        "Apple": null,
-        "Microsoft": null,
-        "Google": 'https://placehold.it/250x250'
-      },
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'https://placehold.it/250x250'
+        },
     });
-  });
+});
+$(function () {
+    $('.icon-bar a').on('click', function () {
+        $(this).parent().find('a.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+function visibilidade(id) {
+    if (document.getElementById) {
+        var divid = document.getElementById(id);
+        var divs = document.getElementsByClassName("divsCategorias");
+        for (var i = 0; i < divs.length; i++) {
+            divs[i].style.display = "none";
+        }
+        divid.style.display = "block";
+
+
+    }
+    return false;
+}
+
+function procurarProfissional(codigoServico) {
+
+    window.location.href = location.origin + '/maridoDeAluguel/set/' + codigoServico;
+}
+function solicitarOrcamento() {
+
+    window.location.href = location.origin + '/maridoDeAluguel/orcamento';
+}
--- src/Controller/ServicoController.php
+++ src/Controller/ServicoController.php
@@ -5,15 +5,48 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Servicos;
+use App\Entity\Categoriasservicos;
+use Doctrine\ORM\QueryBuilder;
 
-class ServicoController extends Controller{
+class ServicoController extends Controller {
+
+    public $em;
 
     /**
      * @Route("/solicitar", name="solicitarProfissional")
      */
     public function solicitarProfissional() {
-        return $this->render('solicitarServico.html.twig');
-        
+        //query builder
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+        //s e c sao alias 
+        $qb->select('s,c')
+                ->from('App\Entity\Servicos', 's')
+                ->join('s.categoriasservicoscategoriasservicos', 'c');
+
+        // to get just one result:
+        // $product = $qb->setMaxResults(1)->getOneOrNullResult();
+        $servicos = $qb->getQuery()->getResult();
+
+        $categorias = $this->getDoctrine()->getRepository(Categoriasservicos::class)
+                ->findAll();
+        return $this->render('solicitarServico.html.twig', array('servicos' => $servicos, 'categorias' => $categorias));
+    }
+
+    /**
+     * @Route("/set/{idservico}", name="set")
+     */
+    public function set($idservico) {
+        $this->get('session')->set('idservicosolicitado', $idservico);
+        return $this->redirectToRoute('procurarprofissional');
+    }
+
+    /**
+     * @Route("/orcamento", name="orcamento")
+     */
+    public function orcamento() {
+        return $this->render('orcamento.html.twig');
     }
 
 }
